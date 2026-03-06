4c5ea9fe2b107c547d0d028c532ac847da03048b
Jetulio <joo_ramos2006@hotmail.com>
2026-03-06 18:40:18 -0300

    feat: base functional mvc

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -13,23 +13,28 @@
 </head>
 
 <body class="min-h-screen flex flex-col bg-base-200 font-sans">
-    <x-layout>
-        <x-slot:title>
-            Welcome
-        </x-slot:title>
-
-        <div class="max-w-2xl mx-auto">
+<x-layout>
+    <x-slot:title>
+        Welcome
+    </x-slot:title>
+    <div class="max-w-2xl mx-auto">
+        @forelse ($chirps as $chirp)
             <div class="card bg-base-100 shadow mt-8">
                 <div class="card-body">
                     <div>
-                        <h1 class="text-3xl font-bold">Welcome to Chirper!</h1>
-                        <p class="mt-4 text-base-content/60">This is your brand new Laravel application. Time to make it
-                            sing (or chirp)!</p>
+                        <div class="font-semibold"> {{ $chirp->user ? $chirp->user->name : 'Anonymous' }}</div>
+                        <div class="mt-1">{{ $chirp->message }}</div>
+                        <div class="text-sm text-gray-500 mt-2">
+                            {{ $chirp->created_at->diffForHumans() }}
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
-    </x-layout>
+        @empty
+            <p class="text-gray-500">No chirps yet. Be the first to chirp!</p>
+        @endforelse
+    </div>
+</x-layout>
 </body>
 
 </html>
